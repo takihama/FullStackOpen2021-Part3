9a73e43331151424754ed16f596c4d0dd73bb212
takihama <danielcalderonuns@gmail.com>
2022-03-27 10:36:28 -0300

    3.10 - Full stack deployment

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ let persons = [
 const app = express()
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 morgan.token('body', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : '')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
